40c56bf3b68ea9ce29336ef07c1b01bf17679da5
zvn2060 <zvn2060@gmail.com>
2019-05-24 22:03:57 +0800

    ready

---
+++ README.md
@@ -0,0 +1,60 @@
+Final Project Note
+===
+# Demo : 6/24
+# 模板
+* Resource
+    管理字型、圖片、音檔，自動控管記憶體
+
+* Game Engine
+    管理遊戲視窗、場景、使用者控制事件 
+
+* IScene
+    場景 虛擬類 需要繼承並覆寫其中的方法
+
+* IObject
+    圖片、圖形控制
+    * Image 
+        圖片格式
+        * sprite
+            圖片操縱精靈
+    * Label
+        靜態文字物件
+    * ImageButton
+        可變圖片按鈕    
+* IControl
+    事件處理
+# 專案結構
+* Final Project
+    * include                       **header folder**
+        * Scenes                    **場景**
+    * src                           **source folder**
+        * Scenes                    **場景**
+    * game
+        * FinalProject.exe
+        * resource                  **resource folder**
+    * .gitignore
+    * README.md
+# TODO
+* basic
+- [ ] Stating Scene
+- [ ] Use images
+- [ ] Music and Sound Effect
+- [ ] Keybord and Mouse
+* Functional Bonus
+- [ ] Animation, Partcle effects 
+- [ ] File save/read (高分榜...)
+- [ ] Two Player/Split screens
+- [ ] Special level design (Super Boss)
+- [ ] AI of enemy(Q-learning)
+- [ ] Physical engine
+- [ ] Fun, Creative, Peformace
+* Code Bonus
+- [X] Git
+- [ ] Smart Pointer
+- [ ] Multi-thread smooth loading (pthread, std::thread…)
+- [ ] Online Multiplayer (sockets…)
+- [ ] Elegant Coding Style (OOP, Design Patterns…)
+- [ ] Try-catch and Lambda function
+- [ ] Algorithms not covered in this course (A* path finding, Separate-Axis Theorem…)
+
+
---
+++ cmake-build-debug/CMakeFiles/Mini2.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Documents/I2P 2/Mini2/src/Allegro5Exception.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Allegro5Exception.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/AudioHelper.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/AudioHelper.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Bullet.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Bullet.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Collider.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Collider.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/DirtyEffect.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/DirtyEffect.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Enemy.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Enemy.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/ExplosionEffect.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/ExplosionEffect.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/FireBullet.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/FireBullet.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/GameEngine.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/GameEngine.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Group.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Group.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/IControl.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/IControl.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/IObject.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/IObject.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/IScene.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/IScene.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Image.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Image.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/ImageButton.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/ImageButton.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/LOG.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/LOG.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Label.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Label.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/LaserBullet.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/LaserBullet.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/LaserTurret.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/LaserTurret.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/MachineGunTurret.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/MachineGunTurret.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/MissileBullet.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/MissileBullet.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/MissileTurret.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/MissileTurret.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Plane.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Plane.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/PlaneEnemy.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/PlaneEnemy.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Point.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Point.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Resources.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Resources.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Scenes/LoseScene.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Scenes/LoseScene.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Scenes/PlayScene.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Scenes/PlayScene.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Scenes/StageSelectScene.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Scenes/StageSelectScene.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Scenes/WinScene.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Scenes/WinScene.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Slider.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Slider.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/SoldierEnemy.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/SoldierEnemy.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Sprite.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Sprite.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/TankEnemy.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/TankEnemy.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/Turret.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/Turret.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/TurretButton.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/TurretButton.cpp.obj"
+  "D:/Documents/I2P 2/Mini2/src/main.cpp" "D:/Documents/I2P 2/Mini2/cmake-build-debug/CMakeFiles/Mini2.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../allegro5/include"
+  "../include"
+  "../include/Scenes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
